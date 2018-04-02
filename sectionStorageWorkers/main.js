f75cbd515c5b531546503f195081be7aa93bd904
Vane Kravitz <vane0kravitz@gmail.com>
2018-04-02 18:57:18 +0300

    session and local storage

---
+++ sectionStorageWorkers/main.js
@@ -0,0 +1,36 @@
+function init() {
+    let result = document.getElementById('result'),
+        checkL = document.getElementById('checkL'),
+        checkS = document.getElementById('checkS'),
+        inputVal = document.getElementById('inputVal'),
+        setLVal = document.getElementById('setLVal'),
+        setSVal = document.getElementById('setSVal'),
+        existS = document.getElementById('existS'),
+        existL = document.getElementById('existL');
+
+    setInterval(function () {
+        if (undefined !== window.localStorage.testKey) {
+            existL.innerHTML = window.localStorage.testKey;
+        }
+
+        if (undefined !== window.sessionStorage.testKey) {
+            existS.innerHTML = window.sessionStorage.testKey;
+        }
+    }, 100);
+
+    checkL.onclick = function () {
+        result.innerHTML = 'Local storage - ' + (window.localStorage ? 'ok' : 'not support');
+    };
+    checkS.onclick = function () {
+        result.innerHTML = 'Session storage - ' + (window.sessionStorage ? 'ok' : 'not support');
+    };
+
+    setLVal.onclick = function () {
+        window.localStorage.testKey = inputVal.value;
+    };
+    setSVal.onclick = function () {
+        window.sessionStorage.testKey = inputVal.value;
+    };
+}
+
+window.addEventListener('load', init, false);
